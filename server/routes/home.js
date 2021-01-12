6eb583731f85041f5cd002c9160aafdc05efc75b
Seow Li Chong Glen <glenseowlc@hotmail.com>
2021-01-12 10:18:18 +0800

    added profile, added change profile picture functionality

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,20 +1,50 @@
 const router = require("express").Router();
 const pool = require('../db');
 const authorization = require('../middleware/authorization');
+const path = require('path');
 
 router.get('/', authorization, async (req, res) => {
     try {
         
-        const user = await pool.query("SELECT user_name FROM users WHERE user_id = $1", [
+        const res_array = [];
+
+        const user = await pool.query("SELECT * FROM users WHERE user_id = $1", [
             req.user
         ]);
 
-        res.json(user.rows[0]);
+        const profile_id = await pool.query("SELECT * FROM profile WHERE profile_id = $1", [
+            user.rows[0].profile_id
+        ]);
+
+        res_array.push(user.rows[0]);
+        res_array.push(profile_id.rows[0]);
+        // res.json(user.rows[0]);
+        res.json(res_array);
 
     } catch (err) {
         console.error(err.message);
         res.status(500).json("Server Error");
     }
-})
+});
+
+router.get('/get-photo', authorization, async (req, res) => {
+    try {
+
+        const user = await pool.query("SELECT * FROM users WHERE user_id = $1", [
+            req.user
+        ]);
+
+        const profile = await pool.query("SELECT profile_picture FROM profile WHERE profile_id = $1", [
+            user.rows[0].profile_id
+        ]);
+
+        const path_name = '.' + profile.rows[0].profile_picture;
+        res.sendFile(path.join(__dirname, path_name));
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json("Server Error");
+    }
+});
 
 module.exports = router;
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -5,6 +5,10 @@ const authorization = require("../middleware/authorization");
 const multer  = require('multer');
 const upload = multer({ dest: './photos/' })
 const fs = require('fs')
+const jwtGenerator = require('../utils/jwtGenerator');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const path = require('path');
 
 router.get('/', authorization, async (req, res) => {
     try {
@@ -21,15 +25,54 @@ router.get('/', authorization, async (req, res) => {
         console.error(err.message);
         res.status(500).json("Server Error");
     }
-})
+});
+
+router.get('/get-photo', authorization, async (req, res) => {
+    try {
+
+        const user = await pool.query("SELECT * FROM users WHERE user_id = $1", [
+            req.user
+        ]);
+
+        const profile = await pool.query("SELECT profile_picture FROM profile WHERE profile_id = $1", [
+            user.rows[0].profile_id
+        ]);
+
+        const path_name = '.' + profile.rows[0].profile_picture;
+        res.sendFile(path.join(__dirname, path_name));
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json("Server Error");
+    }
+});
 
 router.post('/upload-image', upload.single('file'), async (req, res) => {
     try {
+
+        const jwtToken = req.header("token");
+
+        if (!jwtToken) {
+            return res.status(403).json("Not Authorized");
+        }
+
+        const payload = jwt.verify(jwtToken, process.env.jwtSecret);
+
         let fileType = req.file.mimetype.split("/")[1];
         let newFileName = './photos/' + req.file.filename + "." + fileType;
         let oldFileName = './photos/' + req.file.filename;
         fs.rename(oldFileName, newFileName, () => console.log('callback'));
-        res.send('200');
+
+        const profile_id = await pool.query("SELECT (profile_id) FROM users WHERE user_id = $1", [
+            payload.user
+        ])
+
+        const fName = await pool.query("UPDATE profile SET profile_picture = ($1) WHERE profile_id = ($2)", [
+            newFileName,
+            profile_id.rows[0].profile_id
+        ]);
+        res.json({ fName });
+
     } catch (err) {
         console.error(err.message);
         res.status(500).json("Server Error");
