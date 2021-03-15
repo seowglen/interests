95cb46410c32554921c835b9aff3dfc2ad864a67
Seow Li Chong Glen <glenseowlc@hotmail.com>
2021-03-15 15:28:17 +0800

    updated frontend for login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,60 +1,156 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import img from './Simplified Logo (font rasterized).png';
 import './Login.css';
-import { Button } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
 
-const Login = ({ setAuth }) => {
-
-    const [inputs, setInputs] = useState({
-        email: "",
-        password: ""
-    });
-
-    const {email, password} = inputs;
-
-    const onChange = (e) => {
-        setInputs({ ...inputs, [e.target.name]: e.target.value});
-    };
-
-    const onSubmitForm = async (e) => {
-        e.preventDefault()
-        const body = {email, password};
-        try {
-            const response = await fetch("http://localhost:5000/auth/login", {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(body)
-            })
-            
-            const parseRes = await response.json();
-            localStorage.setItem("token", parseRes.token);
-            setAuth(true);
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
-
-    return (
-        <div className="login">
-            <div className='login__logo'>
-                <img src={img} alt=''></img>
-            </div>
-            <form onSubmit={onSubmitForm}>
-                <Typography component="h1" variant="h5">
-                    Login
-                </Typography>
-                <TextField variant="outlined" margin="normal" required fullWidth autoFocus type="email" name="email" placeholder="email" value={email} onChange={e => onChange(e)} />
-                <TextField variant="outlined" margin="normal" required fullWidth type="password" name="password" placeholder="password" value={password} onChange={e => onChange(e)}/>
-                <Button fullWidth variant="contained" type="submit">Login</Button>
-            </form>
-            <Button fullWidth variant="contained">
-                <Link to="/register">Register</Link>
-            </Button>
-        </div>        
-    )
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © CoFriends'}
+      {' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Login;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: "#ffb6c1",
+    color: "#ffffff",
+    "&:hover": {
+        backgroundColor: "#f7196e"
+    }
+  },
+}));
+
+export default function SignIn({ setAuth }) {
+
+  const classes = useStyles();
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: ""
+  });
+
+  const {email, password} = inputs;
+
+  const onChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value});
+  };
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault()
+    const body = {email, password};
+    try {
+        const response = await fetch("http://localhost:5000/auth/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(body)
+        })
+        
+        const parseRes = await response.json();
+        localStorage.setItem("token", parseRes.token);
+        setAuth(true);
+    } catch (err) {
+        console.error(err.message);
+    }
+  }
+
+  return (
+    <Container maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        {/* <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar> */}
+        <img src={img} alt='' style={{objectFit: "contain", height: "100px", marginBottom: "25px"}}></img>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form className="login__form" noValidate onSubmit={onSubmitForm}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            value={email}
+            autoComplete="email"
+            autoFocus
+            onChange={e => onChange(e)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            value={password}
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={e => onChange(e)}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            // color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
